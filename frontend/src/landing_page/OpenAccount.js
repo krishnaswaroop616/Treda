69907dce7656d0d3a50a6301bbdf027640a39741
krishnaswaroop616 <kswaroop311@gmail.com>
2025-03-31 15:27:18 +0530

    Update OpenAccount.js

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -6,7 +6,7 @@ function OpenAccount(){
             <div className='row text-center'>
                 <h1 >Start your trading journey today</h1>
                 <p>open an account in minutes and take control of your investments.</p>
-                <a className='p-2 btn btn-success fs-5 mb-5' style={{width:"20%", margin:"0 auto"}} href='/signup'>Sign Up Now</a>
+                <a className='p-2 btn btn-success fs-5 mb-5' style={{width:"20%", margin:"0 auto"}} href='/Signup'>Sign Up Now</a>
             </div>
         </div>
     );
