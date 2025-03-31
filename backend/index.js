e4a615b950a6329e2de88c4fdaba1df392ae619f
krishnaswaroop616 <kswaroop311@gmail.com>
2025-03-31 15:43:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -257,11 +257,6 @@ app.post("/newOrder", async (req, res) => {
     }
 })
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 app.listen(PORT, () => {
